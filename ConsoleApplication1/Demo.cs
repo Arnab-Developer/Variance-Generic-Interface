389ab0541f19fd2ba1951b634981145645956371
Arnab Roy Chowdhury <arnab-roy-chowdhury@outlook.com>
2013-04-07 12:08:36 +0530

    Initial commit

---
+++ ConsoleApplication1/Demo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Demo<T> : IGetData<T>, ISetData<T>
+    {
+        private T _myValue;
+
+        T IGetData<T>.MyValue
+        {
+            get { return _myValue; }
+        }
+
+        T ISetData<T>.MyValue
+        {
+            set { _myValue = value; }
+        }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Contravariant.
+            ISetData<string> set;
+            set = new Demo<object>();
+
+            // Covariant.
+            IGetData<object> get;
+            get = new Demo<string>();
+        }
+    }
+}
